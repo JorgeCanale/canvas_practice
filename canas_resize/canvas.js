68ce0ab53c7922b41dad94c8c018334fe2448237
Jorge Canale <jorgecanale636@yahoo.com>
2025-06-15 14:06:57 -0300

    clearing the comments

--- canas_resize/canvas.js
+++ canas_resize/canvas.js
@@ -96,55 +96,6 @@ function Circle (x,y,radius,dx,dy,color){
 
 
 
-// ctx.fillStyle = randomColor();
-
-// ctx.fillRect(100, 100, 100, 100);
-
-// ctx.fillStyle = randomColor();
-
-// ctx.fillRect(1000, 100, 100, 100);
-
-// ctx.fillStyle = randomColor();
-
-// ctx.fillRect(570, 300, 100, 100);
-
-// dibujando una linea
-// Con este metedo le digo a canvas que va a empezar un unuevo "camino" o "linea" 
-//el cual no esta ligado a ningun dibujado anterior
-// ctx.beginPath();
-
-//el metodo "moveTo" toma por argumento las coordenadas x e y desde la cual se va a 
-//empezar a trazar la linea, es decir "me muevo" hacia esas coordenadas
-// ctx.moveTo(50,300);
-
-//el metodo "lineTo" recibe como argumento las coordenadas x e Y y trza una linea
-//desde el punto inicial que marcamos anterior mente hacia las coordenadas nuevas
-// ctx.lineTo(100, 350);
-// ctx.lineTo(200, 350);
-// ctx.lineTo(60, 200);
-
-//el metodo "strokeStyle" nos permite personalizar el color del trazo
-// ctx.strokeStyle = randomColor();
-
-//el metodo "stroke" es lo que permite que la linea anterior se reenderize
-// ctx.stroke();
-
-// ctx.beginPath();
-// ctx.arc(400,150,30, 0, Math.PI * 2, false);
-// ctx.strokeStyle = "violet";
-// ctx.stroke();
-
-
-// for(let i = 0; i< 50; i++){
-//     ctx.beginPath();
-//     let x = Math.random() * window.innerWidth;
-//     let y = Math.random() * window.innerHeight;
-//     ctx.arc(x, y, 30, 0, Math.PI * 2, false);
-//     ctx.strokeStyle = randomColor();
-//     ctx.stroke();
-// };
-
-
 let radius = 30;
 let bounce = 0;
 let increaseSpeed = false;
@@ -183,37 +134,8 @@ function animate(){
     
 };
 
-    //on bounce increase speed logic
-    // if(bounce >= 2){
-    //     bounce = 0;     
-    //      if(dx > 0){
-    //         dx++;
-    //      }else{
-    //         dx--
-    //      };
-    //      if(dy > 0){
-    //         dy++
-    //      }else{
-    //         dy--
-    //      };     
-    // };
-
+    
 init();
 animate();
 
-    // let i = 1;
-    // let j = 1;
-
-    // ctx.beginPath();
-    // let xx = 25 + i * 50; // x coordinate
-    // let yy = 25 + i * 50; // y coordinate
-    // let radius2 = 20; // Arc radius
-    // let startAngle = i; // Starting point on circle
-    // let endAngle = Math.PI + (Math.PI * i) / 2; // End point on circle
-    // let counterclockwise = j % 2 === 1; // Draw counterclockwise
-
-    // console.log(endAngle);
-    // ctx.arc(xx, yy, radius2, startAngle, endAngle, true);
-
-    //   ctx.stroke();
 
