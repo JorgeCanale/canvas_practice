3bde2f65cae98bccc8536727fc05c4c570ab8a82
Jorge Canale <jorgecanale636@yahoo.com>
2025-06-19 16:43:57 -0300

    finally the gravity, bounce and friction works as intended but could be better, BETTER

--- canas_resize/gravity balls/canvas.js
+++ canas_resize/gravity balls/canvas.js
@@ -13,6 +13,13 @@ let myColors = [
     '#005C53'
 ];
 
+function reduceFriction (speed){
+    if(Math.abs(speed) > 0.2 && friction > 0.05){
+        friction -= 0.03;
+    }else{
+        friction = 0;
+    }
+}
 
 window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
@@ -55,30 +62,34 @@ function Ball (x,y,radius,dx,dy,color){
 
      this.update = function(){
 
-        if(this.x + this.radius > innerWidth || this.x - this.radius <= 0){
+        if(this.x + this.radius > innerWidth ){
+            this.x = innerWidth - this.radius - 1;
             this.dx = -this.dx * friction;
-            friction -= 0.05; 
-        }else{
-            this
+            reduceFriction(this.dx);
+            console.log("the floor is increasing the friction but like crazy");
+            
+        }else if(this.x - this.radius < 0){
+            this.dx = -this.dx * friction;
+            reduceFriction(this.dx);
         }
 
         if(this.y + this.radius >= innerHeight){
                 this.y = innerHeight - this.radius;
                 this.dy = -this.dy * friction;
-                friction -= 0.05
+                Math.abs(this.dy) > 0.2 ? reduceFriction(this.dy) : this.dy = 0;
                 if(this.maxHeigh > innerHeight - this.radius){
                     this.maxHeigh = innerHeight -this.radius;
                     this.dy = 0;
                 }else{
                     this.maxHeigh +=  Math.floor(Math.abs(this.dy));
                 }
-               //console.log(`Altura: ${this.y} Altura Maxima: ${this.maxHeigh} Velocidad: ${this.dy} Radius: ${this.radius}`)
+               console.log(`posicion: x ${this.x} y ${this.y} ancho:${innerWidth} Velocidad: x - ${this.dx} y - ${this.dy} Friction: ${friction} Radius: ${this.radius}`)
             
         }else{
             this.dy += 1;
         }
 
-        console.log(`posicion: ${this.x} velocidad: ${this.dx}`);
+        
         
         this.x += this.dx;
         this.y += this.dy;
