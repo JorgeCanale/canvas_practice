25f1c849acd7ed091c59ae7a76af87bc24381a43
Jorge Canale <jorgecanale636@yahoo.com>
2025-06-19 23:13:04 -0300

    like 50 balls with phisics (poor ones)

--- canas_resize/gravity balls/canvas.js
+++ canas_resize/gravity balls/canvas.js
@@ -13,12 +13,12 @@ let myColors = [
     '#005C53'
 ];
 
-function reduceFriction (speed){
-    if(Math.abs(speed) > 0.2 && friction > 0.05){
-        friction -= 0.03;
+function reduceFriction (speed,fric){
+    if(Math.abs(speed) > 0.2 && fric > 0.05){
+       return fric - 0.03;
     }else{
-        friction = 0;
-    }
+        return 0; 
+}
 }
 
 window.addEventListener('resize', function(){
@@ -40,13 +40,14 @@ function randomNumber (min,max){
 }
 
 
-function Ball (x,y,radius,dx,dy,color){
+function Ball (x,y,radius,dx,dy,color,fric){
     this.x = x;
     this.y = y;
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
     this.maxHeigh = y - radius;
+    this.energy = fric; 
 
     this.color = color;
 
@@ -64,26 +65,26 @@ function Ball (x,y,radius,dx,dy,color){
 
         if(this.x + this.radius > innerWidth ){
             this.x = innerWidth - this.radius - 1;
-            this.dx = -this.dx * friction;
-            reduceFriction(this.dx);
+            this.dx = -this.dx * this.energy;
+             this.energy = reduceFriction(this.dx, this.energy);
             console.log("the floor is increasing the friction but like crazy");
             
         }else if(this.x - this.radius < 0){
-            this.dx = -this.dx * friction;
-            reduceFriction(this.dx);
+            this.dx = -this.dx * this.energy;
+            this.energy = reduceFriction(this.dx, this.energy);
         }
 
         if(this.y + this.radius >= innerHeight){
                 this.y = innerHeight - this.radius;
-                this.dy = -this.dy * friction;
-                Math.abs(this.dy) > 0.2 ? reduceFriction(this.dy) : this.dy = 0;
+                this.dy = -this.dy * this.energy;
+                Math.abs(this.dy) > 0.2 ? this.energy = reduceFriction(this.dy,this.energy) : this.dy = 0;
                 if(this.maxHeigh > innerHeight - this.radius){
                     this.maxHeigh = innerHeight -this.radius;
                     this.dy = 0;
                 }else{
                     this.maxHeigh +=  Math.floor(Math.abs(this.dy));
                 }
-               console.log(`posicion: x ${this.x} y ${this.y} ancho:${innerWidth} Velocidad: x - ${this.dx} y - ${this.dy} Friction: ${friction} Radius: ${this.radius}`)
+            //    console.log(`posicion: x ${this.x} y ${this.y} ancho:${innerWidth} Velocidad: x - ${this.dx} y - ${this.dy} Friction: ${friction} Radius: ${this.radius}`)
             
         }else{
             this.dy += 1;
@@ -101,21 +102,21 @@ let balls = [];
 let ball;
 
 function init (){
-    // balls = [];
-    // for (let i = 0; i < 50; i++) {
-    //         let dx = (Math.random() + 3); 
-    //         let dy = (Math.random() + 0.9);
-    //         let color = randomColor();
-    //         let radius =  randomNumber(18,30);
-    //         let ball = new Ball(randomNumber(30,innerWidth), randomNumber(30,innerHeight),radius,dx,dy, color);
-    //         balls.push(ball);
-    //     }; 
-
+     balls = [];
+    for (let i = 0; i < 50; i++) {
             let dx = (Math.random() + 3); 
             let dy = (Math.random() + 0.9);
             let color = randomColor();
             let radius =  randomNumber(18,30);
-            ball = new Ball(randomNumber(30,innerWidth), randomNumber(30,innerHeight),radius,dx,dy, color);
+            let ball = new Ball(randomNumber(30,innerWidth), randomNumber(30,innerHeight),radius,dx,dy, color, friction);
+            balls.push(ball);
+        }; 
+
+            // let dx = (Math.random() + 3); 
+            // let dy = (Math.random() + 0.9);
+            // let color = randomColor();
+            // let radius =  randomNumber(18,30);
+            // ball = new Ball(randomNumber(30,innerWidth), randomNumber(30,innerHeight),radius,dx,dy, color, friction);
     }
 
 
@@ -123,12 +124,12 @@ function init (){
 function animate(){
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, innerWidth, innerHeight);
-    // for (let i = 0; i < balls.length; i++) {
-    //     balls[i].draw()
-    //     balls[i].update()
-    // }
-    ball.draw();
-    ball.update()
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].draw()
+        balls[i].update()
+    }
+    // ball.draw();
+    // ball.update();
 };
 
     
